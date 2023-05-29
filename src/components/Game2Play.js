f05256760b8db457b9fd454f2c06b2523fba36aa
E Luckie <eluckie.d@gmail.com>
2023-05-28 20:11:56 -0600

    add "under construction" placeholder for game2

--- src/components/Game2Play.js
+++ src/components/Game2Play.js
@@ -1,10 +1,41 @@
-
+import Banner from "./Banner";
+import NavLinks from "./NavLinks";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function Game2Play() {
+  const divStyles = {
+    textAlign: "center",
+    border: "4px solid black",
+    width: 400,
+    margin: "auto"
+  }
+  
+  const headerStyles = {
+    color: "red"
+  }
+
+  const buttonText = {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "black"
+  }
+
   return (
-    <>
-      <h1>game 2 placeholder</h1>
-    </>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}>
+      <Banner/>
+      <Link to="/play/puzzle"><button><span style={buttonText}>change game</span></button></Link>
+      <br/><br/><br/><br/>
+      <div style={divStyles}>
+        <h1 style={headerStyles}>PAGE UNDER CONSTRUCTION</h1>
+        <h3>slider game coming soon</h3>
+      </div>
+      <NavLinks/>
+      <Banner/>
+    </motion.div>
   );
 }
 
