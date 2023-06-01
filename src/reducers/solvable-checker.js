16c2ed92f4eddab1ca7c252d33ff8532c55db372
E Luckie <eluckie.d@gmail.com>
2023-06-01 13:14:22 -0600

    BUGFIX: solvable-checker for game2 was still allowing unsolvable games to start

--- src/reducers/solvable-checker.js
+++ src/reducers/solvable-checker.js
@@ -3,31 +3,18 @@
 export function isSolvable(difficulty, idArray) {
   let inversionCount = getInversionCount(difficulty, idArray);
 
-  if (difficulty % 2 === 1) {
-    if (inversionCount % 2 === 1) {
-      return false;
-    } else {
-      return true;
-    }
+  if (difficulty & 1) {
+    return !(inversionCount & 1)
   } else {
     // If size is even, the puzzle is solvable if
     //   - inversion count is odd and the blank tile appears on an even row counting from bottom
     //   - inversion count is even and the blank tile appears on an odd row counting from bottom
     let rowFromBottom = findBlankPositionFromBottom(difficulty, idArray);
-    let inversionCount = getInversionCount(difficulty, idArray);
 
-    if (inversionCount % 2 === 1) {
-      if (rowFromBottom % 2 === 1) {
-        return false;
-      } else {
-        return true;
-      }
+    if (rowFromBottom & 1) {
+        return !(inversionCount & 1);
     } else {
-      if (rowFromBottom % 2 === 1) {
-        return true;
-      } else {
-        return false;
-      }
+      return !!(inversionCount & 1);
     }
   }
 }
@@ -35,7 +22,7 @@ export function isSolvable(difficulty, idArray) {
 function getInversionCount(difficulty, idArray) {
   let inversionCount = 0;
   for (let i = 0; i < difficulty * difficulty - 1; i++) {
-    for (let j = 1; j < difficulty * difficulty; j++) {
+    for (let j = i + 1; j < difficulty * difficulty; j++) {
       if (idArray[j] && idArray[i] && idArray[i] > idArray[j]) {
         inversionCount++;
       }
